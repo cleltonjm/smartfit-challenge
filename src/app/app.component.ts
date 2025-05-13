5857538b81e759b711f4f93623bac2903e128b1b
Clelton <cleltonjm_95@hotmail.com>
2025-05-13 09:54:13 -0300

    feat: create forrm structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "./components/header/header.component";
+import { FormsComponent } from "./components/forms/forms.component";
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, FormsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
