669b1c390390325f60d0ec6ee186d5d1d9fffa4a
Clelton <cleltonjm_95@hotmail.com>
2025-05-13 10:37:43 -0300

    feat: adding form control and fix responsivity issues

--- src/app/components/forms/forms.component.ts
+++ src/app/components/forms/forms.component.ts
@@ -1,11 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-forms',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './forms.component.html',
   styleUrl: './forms.component.scss'
 })
-export class FormsComponent {
+export class FormsComponent implements OnInit {
   results = [];
+  formGroup!: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.formGroup = this.formBuilder.group({
+      hour: '',
+      showClosed: false
+    })
+  } 
+
+  onSubmit() {
+    console.log(this.formGroup.value)
+  }
+
+  onClean() {
+    this.formGroup.reset();
+  }
 }
